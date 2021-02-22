53807aeace687d0012b98adcff84deca00757f4b
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-02-22 22:16:08 +0100

    Refactored handling of stats command in own method

--- bot.py
+++ bot.py
@@ -16,10 +16,47 @@ def getAllUsers():
 	server = getServer()
 	return server.members
 
+def command_stats(author, tag, args, message):
+	response = ""
+
+	#Own Stats
+	if(len(args) == 0):
+		results = database.findByUsername(author, tag)
+		if(len(results) == 0):
+			response = "User" + author + "has not sent any messages on this server"
+		else:
+			playerData = results[0]
+			response = "User "+ author+ " has sent "+ str(playerData["messagesSent"]) + " messages so far."
+
+	#Stats of other member
+	elif(len(args) == 1):
+		splitArgument = str(args[0]).split("#")
+		username = splitArgument[0]
+
+		#If tag is included in request
+		if(len(splitArgument) == 2):
+			tag = splitArgument[1]
+			results = database.findByUsername(username, tag)
+			if(len(results) == 0):
+				response = "No users in database matching full username " + username + "#" + tag
+			else:
+				playerData = results[0]
+				response = "User "+ username+ "#" + tag +  " has sent "+ str(playerData["messagesSent"]) + " messages so far."
+
+		#If tag is not included in request, all members with matching username will be selected
+		else:
+			results = database.findByUsername(username)
+			if(len(results) == 0):
+				response = "No users in database matching username " + username
+			for result in results:
+				response = response + "User " + username + "#" + result["tag"] + " has sent " + str(result["messagesSent"]) + " messages so far\n"
+	else:
+		response = "Too many arguments given, use !stats for own stats or !stats <username> for another users stats"
+	return response
+
+
 @bot.event
 async def on_message(message):
-	
-	
 	if message.author == bot.user:
 		return
 
@@ -30,40 +67,7 @@ async def on_message(message):
 	(author,tag) = str(message.author).split("#")[0:2]
 
 	if(command == "!stats"):
-
-		#Own Stats
-		if(len(args) == 0):
-			results = database.findByUsername(author, tag)
-			if(len(results) == 0):
-				response = "User" + author + "has not sent any messages on this server"
-			else:
-				playerData = results[0]
-				response = "User "+ author+ " has sent "+ str(playerData["messagesSent"]) + " messages so far."
-
-		#Stats of other member
-		elif(len(args) == 1):
-			splitArgument = str(args[0]).split("#")
-			username = splitArgument[0]
-
-			#If tag is included in request
-			if(len(splitArgument) == 2):
-				tag = splitArgument[1]
-				results = database.findByUsername(username, tag)
-				if(len(results) == 0):
-					response = "No users in database matching full username " + username + "#" + tag
-				else:
-					playerData = results[0]
-					response = "User "+ username+ "#" + tag +  " has sent "+ str(playerData["messagesSent"]) + " messages so far."
-
-			#If tag is not included in request, all members with matching username will be selected
-			else:
-				results = database.findByUsername(username)
-				if(len(results) == 0):
-					response = "No users in database matching username " + username
-				for result in results:
-					response = response + "User " + username + "#" + result["tag"] + " has sent " + str(result["messagesSent"]) + " messages so far\n"
-		else:
-			response = "Too many arguments given, use !stats for own stats or !stats <username> for another users stats"
+		response = command_stats(author, tag, args, message)
 		await message.channel.send(response)
 		return
 
