25230ac1e7b0a273dcd3bf133bd08662d0a66a86
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-03-06 23:06:50 +0100

    Added possibility to restore default configuration entires

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
 import json
 
-def initConfig():
-    #No config options yet, will be added when new functionalities are added
+#This method holds the default values for all configuration options
+#It is used for initializing the config value and restoring entries, if they get deleted from the config.json
+def defaultConfig():
+    #DO NOT EDIT THIS DICTIONARY WHILE IN PRODUCTION
     data = {
-
+        
     }
+    return data
+
+def initConfig():
+    data = defaultConfig()
     with open("config.json", "w+") as jsonfile:
         json.dump(data, jsonfile, indent = 4)
 
@@ -17,7 +23,22 @@ def loadConfigEntries():
 
 def getFromConfig(entry):
     data = loadConfigEntries()
-    return data[entry]
+    try:
+        #If entry exists in config
+        return data[entry]
+    except:
+        #Entry does not exist in config, check if it exists in default configuration
+        print("Not found in config, trying to restore from default config...")
+        defaultData = defaultConfig()
+        try:
+            #If entry does not exist in config, but exists in default configuration (happens by manually deleting entries from config.json)
+            changeConfigEntry(entry, defaultData[entry])
+            print("Argument was not found in config, but in default configuration, restored the entry in config.json to its default value")
+            return defaultData[entry]
+        except:
+            #If entry neither exists in config, nor default configuration, in this case, the given entry argument is not a valid configuration option 
+            print("Not found in default config, this configuration option does not exist")
+            return ""
 
 def changeConfigEntry(entry, newvalue):
     data = loadConfigEntries()
