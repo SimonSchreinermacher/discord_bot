6b95098c4e6f742bdaec85169501f0a502396d01
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-03-06 23:15:14 +0100

    Now only executes config operations, if the config file exists

--- config.py
+++ config.py
@@ -16,32 +16,37 @@ def initConfig():
 
 def loadConfigEntries():
     data = {}
-    with open("config.json", "r") as jsonfile:
-        data = json.load(jsonfile)
-        jsonfile.close()
-    return data
+    try:
+        with open("config.json", "r") as jsonfile:
+            data = json.load(jsonfile)
+            jsonfile.close()
+        return data
+    except:
+        print("Config not initialized yet!")
 
 def getFromConfig(entry):
     data = loadConfigEntries()
-    try:
-        #If entry exists in config
-        return data[entry]
-    except:
-        #Entry does not exist in config, check if it exists in default configuration
-        print("Not found in config, trying to restore from default config...")
-        defaultData = defaultConfig()
+    if(data != None):
         try:
-            #If entry does not exist in config, but exists in default configuration (happens by manually deleting entries from config.json)
-            changeConfigEntry(entry, defaultData[entry])
-            print("Argument was not found in config, but in default configuration, restored the entry in config.json to its default value")
-            return defaultData[entry]
+            #If entry exists in config
+            return data[entry]
         except:
-            #If entry neither exists in config, nor default configuration, in this case, the given entry argument is not a valid configuration option 
-            print("Not found in default config, this configuration option does not exist")
-            return ""
+            #Entry does not exist in config, check if it exists in default configuration
+            print("Not found in config, trying to restore from default config...")
+            defaultData = defaultConfig()
+            try:
+                #If entry does not exist in config, but exists in default configuration (happens by manually deleting entries from config.json)
+                changeConfigEntry(entry, defaultData[entry])
+                print("Argument was not found in config, but in default configuration, restored the entry in config.json to its default value")
+                return defaultData[entry]
+            except:
+                #If entry neither exists in config, nor default configuration, in this case, the given entry argument is not a valid configuration option 
+                print("Not found in default config, this configuration option does not exist")
+                return ""
 
 def changeConfigEntry(entry, newvalue):
     data = loadConfigEntries()
-    data[entry] = newvalue
-    with open("config.json", "w") as jsonfile:
-        json.dump(data, jsonfile, indent=4)
+    if(data != None):
+        data[entry] = newvalue
+        with open("config.json", "w") as jsonfile:
+            json.dump(data, jsonfile, indent=4)
